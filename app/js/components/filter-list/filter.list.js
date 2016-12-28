76f25a0ff21198ceb1b40a524fcfc9f452ec130d
PhutureProof <dale.phutureproof@gmail.com>
2016-12-28 00:44:23 +0000

    Added options column header

--- app/js/components/filter-list/filter.list.js
+++ app/js/components/filter-list/filter.list.js
@@ -111,6 +111,7 @@
                             }
                         }
                     }
+                    headers.push('');
                 }
                 return headers;
             };
